9b6f893eb19554bcadc567e6e2b00e9ce75f1bcf
dhaneshkk <dhaneshkk@gmail.com>
2015-12-20 07:23:17 +0530

    completed string exercise

--- stringExercise.hs
+++ stringExercise.hs
@@ -1,12 +1,23 @@
-module stringExercise where
+module StringExercise where
 
---addBang::String->String
+addBang::String->String
+addBang xs = let s = xs++"!" in s
 
-getNthChar::String->Integer->String
-getNthChar x n = let k = drop 9 ("Curry is awesome"++"!")  in print k
+getStringFromNthChar::String->Int->String
+getStringFromNthChar x n =   let k = drop n x in k 
+
+getNthChar::String->Int->String
+getNthChar xs n = let k = drop n xs  in (head k):[]
+
+rvrs::[Char] -> [Char]
+rvrs [] = []
+rvrs (x:xs) = rvrs xs ++ [x] 
 
 main::IO()
 main = do
-    putStrLn getNthChar "Curry is awesome!" 9
+    putStrLn $ addBang "Curry is awesome"
+    putStrLn $ getNthChar "Curry is awesome!" 4
+    putStrLn $ getStringFromNthChar "Curry is awesome!" 9
+    putStrLn $ rvrs "JaiBhairavi"
 ----let k = ("Curry is awesome"++"!")!!4  in print k
 --let k = "Curry is awesome"++"!"  in print k
